135bce2d950a9aab413a8153eba0fe059240c867
silent987 <tyler.dalton@graspen.com>
2015-06-18 19:55:49 -0500

    First commit of code

---
+++ KcCodeMentors/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace KcCodeMentors
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/js").Include(
+                    "~/js/contact_me.js",
+                    "~/js/jqBootstrapValidation.js",
+                    "~/js/jquery.js",
+                      "~/js/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/modern-business.css"));
+        }
+    }
+}
